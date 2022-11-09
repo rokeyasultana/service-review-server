3565afb4e9c97cbba19a46e642dde0eafee10de3
jakiasultana2022 <rokeyasbd@gmail.com>
2022-11-09 20:50:51 +0600

    get services

--- index.js
+++ index.js
@@ -3,19 +3,48 @@ const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 //middle wares
 app.use(cors());
-app.use(express.json)
+app.use(express.json());
 
 
 //mongodb
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kz5fz2c.mongodb.net/?retryWrites=true&w=majority`;
 
-
-
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run (){
+  try{
+
+    const serviceCollection = client.db('service-review').collection('services');
+
+//get service
+
+    app.get('/services', async (req, res) => {
+      const query = {}
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+  });
+
+
+  }
+
+
+
+  finally{
+
+  }
+
+}
+
+
+
+run().catch(err => console.err(err));
+
 
 
 app.get('/', (req, res) => {
@@ -25,3 +54,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Service Review Website listening on port ${port}`)
 })
+
