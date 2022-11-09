bff2e55accccf137b6e2bae01af23f2f9c602d87
jakiasultana2022 <rokeyasbd@gmail.com>
2022-11-09 20:57:39 +0600

    service id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -30,6 +30,14 @@ async function run (){
       res.send(services);
   });
 
+  //get service by id
+
+  app.get('/services/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: ObjectId(id) };
+    const service = await serviceCollection.findOne(query);
+    res.send(service);
+});
 
   }
 
